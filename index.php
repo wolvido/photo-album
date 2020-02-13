0889c18b97edd65601a22b0c44741639d5c97a97
winzyl <prowinzil@gmail.com>
2020-02-13 13:21:53 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<p>Create Album<P>
+
+<form action="album.php" method="get">
+Album Name: <input type="text" name="name"><br>
+<input  type="submit">
+</form>
+
+<p>Open Album<P>
+<form action="display.php" method="get">
+Album Name: <input type="text" name="name"><br>
+<input  type="submit">
+</form>
+
+</body>
+</html>
